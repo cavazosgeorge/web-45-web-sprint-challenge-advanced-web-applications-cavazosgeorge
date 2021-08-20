68a431c1bb616f10543c37d3f31756aec0684185
George Cavazos <cavazosgeorge@yahoo.com>
2021-08-20 18:30:57 -0400

    Bubble Page Complete and Test | ColorList Complete and Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
   return (
@@ -10,8 +12,11 @@ function App() {
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          {/* <a data-testid="logoutButton" href="#" onClick={handleLogout}>logout</a> */}
+          <LogoutButton />
         </header>
+        <PrivateRoute path="/bubbles" component={BubblePage} />
+        <Route exact path="/" component={Login} />
       </div>
     </Router>
   );
--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,69 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
+import { fetchColorService as mockFetch } from "../services/fetchColorService";
+import { render, screen, waitFor } from "@testing-library/react";
+import BubblePage from "./BubblePage";
 
-import { render, screen} from "@testing-library/react";
-import BubblePage from './BubblePage';
+jest.mock("../services/fetchColorService");
 
-test("Renders without errors", ()=> {
-    
+const colors = {
+  data: [
+    {
+      color: "aliceblue",
+      code: {
+        hex: "#f0f8ff",
+      },
+      id: 1,
+    },
+    {
+      color: "limegreen",
+      code: {
+        hex: "#99ddbc",
+      },
+      id: 2,
+    },
+    {
+      color: "aqua",
+      code: {
+        hex: "#00ffff",
+      },
+      id: 3,
+    },
+    {
+      color: "aquamarine",
+      code: {
+        hex: "#7fffd4",
+      },
+      id: 4,
+    },
+    {
+      color: "lilac",
+      code: {
+        hex: "#9a99dd",
+      },
+      id: 5,
+    },
+  ],
+};
+
+test("Renders without errors", async () => {
+  mockFetch.mockResolvedValueOnce([]);
+  render(<BubblePage />);
+  await waitFor(() => {
+    const colors = screen.getByText(/colors/i);
+    const bubbles = screen.getByText(/bubbles/i);
+    expect(colors).toHaveTextContent("Colors");
+    expect(bubbles).toHaveTextContent("Bubbles");
+  });
 });
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this component.
+test("Renders appropriate number of colors passed in through mock", async () => {
+  //Keep in mind that our service is called on mount for this component.
+  mockFetch.mockResolvedValueOnce(colors);
+  render(<BubblePage />);
+  await waitFor(() => []);
+  const list = screen.getAllByTestId("color");
+  const lilac = screen.getByText(/limegreen/i);
+  expect(list).toHaveLength(5);
+  expect(lilac).toBeInTheDocument();
 });
